c1dcacfb0fae575f2628d1a96949d066de45fa0f
jjanuszczak <john@johnjanuszczak.com>
2018-04-29 20:17:18 +0800

    First commit

---
+++ R/api.R
@@ -0,0 +1,23 @@
+# Freshdesk API Utilities
+
+freshdesk_client <- function(domain, api_key, password = "x") {
+  config <- list(domain = domain, api_key = api_key, password = password)
+  return(config)
+}
+
+freshdesk_api <- function(client, path) {
+  url <- modify_url(paste0("https://", client$domain, ".freshdesk.com"), path = path)
+  resp <- httr::GET(url, authenticate(client$api_key, client$password))
+
+  # send a worning if we don't get success
+  if (httr::http_status(resp)$category != "Success"){
+    warning(paste0(httr::http_status(resp)$reason," ",httr::http_status(resp)$message))
+  }
+
+  # send a warning if we don't get json back
+  if (httr::http_type(resp) != "application/json") {
+    warning("API did not return json", call. = FALSE)
+  }
+
+  return(resp)
+}
