96ce434e44ec7da08d8e5e35503c2c12f2c7fe7c
jjanuszczak <john@johnjanuszczak.com>
2018-05-01 18:05:25 +0800

    Adding internal lookup tables for Tickets

---
+++ data-raw/lookup_tables.R
@@ -0,0 +1,21 @@
+# lookup tables useful for interacting with the Freshdesk API
+
+# create priorities lookup table
+values <- c(1, 2, 3, 4)
+priority <- c("Low", "Medium", "High", "Urgent")
+ticket_priorities <- data.frame("Priority" = priority, "Value" = values)
+
+# creates the sources lookup table
+values <- c(1, 2, 3, 7, 8, 9, 10)
+source <- c("Email", "Portal", "Phone", "Chat", "Mobihelp", "Feedback Widget", "Outbound Email")
+ticket_sources <- data.frame("Source" = source, "Value" = values)
+
+# create ticket status lookup table
+values <- c(2, 3, 4, 5)
+status <- c("Open", "Pending", "Resolved", "Closed")
+ticket_status <- data.frame("Status" = status, "Value" = values)
+
+# persist the lookup tables for internal package use
+devtools::use_data(ticket_priorities, internal = TRUE, overwrite = TRUE)
+devtools::use_data(ticket_sources, internal = TRUE, overwrite = TRUE)
+devtools::use_data(ticket_status, internal = TRUE, overwrite = TRUE)
