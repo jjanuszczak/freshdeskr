0024f548eaa9a454107362cab8a213106f32571b
jjanuszczak <john@johnjanuszczak.com>
2018-05-04 20:07:17 +0800

    Modifes Ticket function to remove hard-coded dependcies
    Closes #7 by adding the lookup tables and vector of valid include query parameters as arguments to the `ticket` function. These arguments default to the appropriate lookups and vector predefined in the package.

--- R/tickets.R
+++ R/tickets.R
@@ -13,6 +13,14 @@
 #' @param tickets_path The path of the tickets API. Defaults to \code{/api/v2/tickets}.
 #'   You should not need to change the default value.
 #' @param include Optional include a vector of query string parameters.
+#' @param valid_include_parameters Optional vector of valid include parameters. Defaults to
+#'   \code{ticket_include_parameters} which is defined in the package.
+#' @param priorities_lookup Optional dataframe of ticket priorities and associated values.
+#'   Defaults to \code{ticket_priorities} which is defined in the package.
+#' @param sources_lookup Optional dataframe of ticket sources and associated values.
+#'   Defaults to \code{ticket_sources} which is defined in the package.
+#' @param status_lookup Optional dataframe of ticket status and associated values.
+#'   Defaults to \code{ticket_status} which is defined in the package.
 #' @return A list with the attributes of the ticket.
 #' @examples
 #' \dontrun{
@@ -22,7 +30,14 @@
 #' t <- ticket(fc, 3, include = "requester,company")
 #' }
 #' @export
-ticket <- function(client, ticket_id, tickets_path = "/api/v2/tickets", include = NULL) {
+ticket <- function(client,
+                   ticket_id,
+                   tickets_path = "/api/v2/tickets",
+                   include = NULL,
+                   valid_include_parameters = ticket_include_parameters,
+                   priorities_lookup = ticket_priorities,
+                   sources_lookup = ticket_sources,
+                   status_lookup = ticket_status) {
   # validate arguments
   if (is.null(ticket_id)) {
     stop("Ticket ID not specified", call. = FALSE)
@@ -30,7 +45,7 @@ ticket <- function(client, ticket_id, tickets_path = "/api/v2/tickets", include 
 
   # add optional include query parameters
   if (!is.null(include)) {
-    if (!all(include %in% ticket_include_parameters)) {
+    if (!all(include %in% valid_include_parameters)) {
       stop("Invalid parameters for include quesry string", call. = FALSE)
     } else {
       include <- paste(include, collapse = ",")
@@ -46,9 +61,9 @@ ticket <- function(client, ticket_id, tickets_path = "/api/v2/tickets", include 
   ticket_data <- apidata$content
 
   # do some cleaning
-  ticket_data$priority = ticket_priorities$Priority[ticket_priorities$Value == ticket_data$priority]
-  ticket_data$source = ticket_sources$Source[ticket_sources$Value == ticket_data$source]
-  ticket_data$status = ticket_status$Status[ticket_status$Value == ticket_data$status]
+  ticket_data$priority = priorities_lookup$Priority[priorities_lookup$Value == ticket_data$priority]
+  ticket_data$source = sources_lookup$Source[sources_lookup$Value == ticket_data$source]
+  ticket_data$status = status_lookup$Status[status_lookup$Value == ticket_data$status]
 
   return(ticket_data)
 }
