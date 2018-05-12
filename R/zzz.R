9072ab48edd56f588c4913d43bc1c4f22cf2ea47
jjanuszczak <john@johnjanuszczak.com>
2018-05-12 21:58:39 +0800

    Fixes NOTES generated from devtools::check()
    Adds non-package scripts in working directory to .Rbuildignore. Solves "no visible binding for global variable [variable name]" NOTE by using utils::globalVariables hack (see zzz.R).

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+#' @importFrom utils globalVariables
+if(getRversion() >= "2.15.1")  {
+  utils::globalVariables(c('ticket_priorities', 'ticket_sources', 'ticket_status', 'ticket_include_parameters', 'ticket_date_fields'))
+}
--- data-raw/field_lists.R
+++ data-raw/field_lists.R
@@ -4,4 +4,4 @@
 ticket_date_fields <- c("due_by", "fr_due_by", "created_at", "updated_at")
 
 # persist the lists for internal package use
-devtools::use_data(ticket_date_fields, internal = FALSE, overwrite = TRUE)
+devtools::use_data(ticket_date_fields, internal = FALSE, overwrite = TRUE, pkg = )
